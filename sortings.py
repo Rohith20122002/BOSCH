582861592555a41fb8ca67f34618e0234012499b
rohith <sairohitha1@gmail.com>
2025-08-29 12:33:42 +0530

    sotings file added

---
+++ sortings.py
@@ -0,0 +1,55 @@
+def linear(a,m):
+    for i in range(len(a)):
+        if(m==a[i]):
+           print("target found")
+        else:
+            print("not found")
+m=30
+linear(a,m)
+
+def binary_search(a, target):
+    a.sort()  # If your list is not sorted already
+    left, right = 0, len(a) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if a[mid] == target:
+            print("target found at index", mid)
+            return mid
+        elif a[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    print("target not found")
+    return -1
+
+# Example usage:
+a = [10, 3, 6, 8, 2]
+m = 6
+binary_search(a, m)
+
+
+
+
+
+
+a=[1,3,2,4,85,34,7,8]
+print(a)
+def bubble(a):
+    for i in range(0,len(a)):
+        for i in range(0,len(a)-1):
+            if(a[i]>a[i+1]):
+               a[i],a[i+1]=a[i+1],a[i]
+    print(a)
+bubble(a)
+
+
+a=[1,3,2,4,85,34,7,8]
+def selection(a):
+    for i in range(0,len(a)):
+        for i in range(0,len(a)-1):
+            if(a[i]>a[i+1]):
+               a[i+1],a[i]=a[i],a[i+1]
+    print(a)
+selection(a)
