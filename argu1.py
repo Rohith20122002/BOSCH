a0b935d8e922d882b8b43d57160b7dc492d08b40
rohith <sairohitha1@gmail.com>
2025-09-09 14:38:21 +0530

    gitupdate

--- argu1.py
+++
@@ -1,7 +0,0 @@
-import sys
-args = sys.argv[1:]
-numeric_args = [arg for arg in args ]
-biggest = max(numeric_args)
-smallest = min(numeric_args)
-print("Biggest value:" ,biggest)
-print("Smallest value: ",smallest)
